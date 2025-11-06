5ba40787375053aeabdb6ed53399a6720cf48f8b
Divyanshi Naithani <naithani1604@gmail.com>
2025-11-06 17:08:37 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import { motion } from "framer-motion";
+import Navbar from "../components/Navbar";
+import Home from "../components/Home";
+import HeroSection from "../components/HeroSection";
+import Vision from "../components/Vision";
+import ServicesGrid from "../components/ServicesGrid";
+import Digital from "../components/Digital";
+import Footer from "../components/Footer";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Navbar />
+      <main className="flex flex-col items-center justify-center text-center">
+        <Home />
+        <ServicesGrid />
+        <HeroSection />
+        <Vision />
+        <Digital />
+      </main>
+      <Footer />
+    </div>
+  );
+}
